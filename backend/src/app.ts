1311c8597396f0debfb7f4c2a4fcb2729d5306a2
Narottam <coutkunal@gmail.com>
2023-02-21 12:38:25 +0530

    added cors options for capacitor

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -18,7 +18,29 @@ const PORT: Number = parseInt(process.env.PORT as string, 10);
 
 const app = express();
 
-app.use(cors());
+
+const allowedOrigins = [
+  'capacitor://localhost',
+  'ionic://localhost',
+  'http://localhost',
+  'http://localhost:8080',
+  'http://localhost:8100',
+];
+
+// Reflect the origin if it's in the allowed list or not defined (cURL, Postman, etc.)
+const corsOptions = {
+  // @ts-expect-error
+  origin: (origin, callback) => {
+    if (allowedOrigins.includes(origin) || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('Origin not allowed by CORS'));
+    }
+  },
+};
+
+// Enable preflight requests for all routes
+app.options('*', cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
