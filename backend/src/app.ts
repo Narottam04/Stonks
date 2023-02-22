55acc08aaeb36ae6828f42d583755a84dcb79228
Narottam <coutkunal@gmail.com>
2023-02-22 23:16:44 +0530

    added local dev server in cors

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -25,6 +25,7 @@ const allowedOrigins = [
   'http://localhost',
   'http://localhost:8080',
   'http://localhost:8100',
+  'http://localhost:5173'
 ];
 
 // Reflect the origin if it's in the allowed list or not defined (cURL, Postman, etc.)
