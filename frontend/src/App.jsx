ea4df931af6df7baaa645eb0865b9760d81097b2
Narottam <coutkunal@gmail.com>
2023-05-08 04:06:43 +0530

    added Error boundary

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,7 +5,8 @@ import "./App.css";
 import AnimatedRoutes from "./Components/AnimatedRoutes";
 import ScrollToTop from "./Components/ScrollToTop";
 import { useGetCurrencyConversionsQuery } from "./services/coinsDataApi";
-
+import ErrorBoundary from "./Utils/ErrorBoundary";
+import ClientError from "./Pages/ClientError";
 
 function App() {
   const { data, error, isLoading, isSuccess } = useGetCurrencyConversionsQuery();
@@ -16,8 +17,10 @@ function App() {
   return (
     <div className="App scrollbar scrollbar-thumb-gray-900 scrollbar-track-gray-100 bg-black">
       <BrowserRouter>
-        <ScrollToTop />
-        <AnimatedRoutes />
+        <ErrorBoundary fallback={<ClientError />}>
+          <ScrollToTop />
+          <AnimatedRoutes />
+        </ErrorBoundary>
       </BrowserRouter>
     </div>
   );
